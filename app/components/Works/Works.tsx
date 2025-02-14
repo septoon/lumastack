b5c1894bfe318e0e8fe0d4c2754e93b40d3c56ff
Tigran <41443306+septoon@users.noreply.github.com>
2025-02-14 15:51:23 +0300

    fix useState

--- app/components/Works/Works.tsx
+++ app/components/Works/Works.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Works = () => {
   const [prefersDark, setPrefersDark] = useState(false);
